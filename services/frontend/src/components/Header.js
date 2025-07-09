732566624f2e06b74f6546ce929c4984f5bb0f39
Katrina <katrinabodani404@gmail.com>
2025-07-09 15:19:26 +0500

    Made adjustments in layout

--- services/frontend/src/components/Header.js
+++ services/frontend/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header({ darkMode, toggleDarkMode }) {
   return (
     <div className="flex items-center justify-between mb-4">
       <h1 className="text-3xl font-bold text-blue-700 dark:text-blue-300">
-        Your Hunza Guide🏔️
+        SWISS TRAVEL BUDDY
       </h1>
       <DarkModeToggle darkMode={darkMode} setDarkMode={toggleDarkMode} />
     </div>
